2978fbb79722761cae016cce8c57144cddf53580
jmigsan <43961001+jmigsan@users.noreply.github.com>
2024-10-09 10:26:00 +0100

    made it a trigger instead

--- Assets/Scripts/Monster/MonsterCastleCollisionDetector.cs
+++ Assets/Scripts/Monster/MonsterCastleCollisionDetector.cs
@@ -6,9 +6,9 @@ public class MonsterCastleCollisionDetector : MonoBehaviour
 {
     // When monster collider collides with object with 'Castle' tag, it sends a log to console.
     // Does nothing yet.
-    void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(UnityEngine.Collider other)
     {
-        if (collision.gameObject.CompareTag("Castle"))
+        if (other.gameObject.CompareTag("Castle"))
         {
             Debug.Log("castle has been hit by monster");
         }
